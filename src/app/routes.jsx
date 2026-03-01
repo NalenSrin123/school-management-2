133c5210d4f16e2e47d9dab7b8c77c0ef4329db3
Reaksmey396 <khimreaksmey123@gmail.com>
2026-03-01 18:56:37 +0700

    Design_page_get_template_reset_password_in_email

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Home from '../public-site/pages/Home'
 import Login from '../dashboard/pages/auth/Login'
+import ResetPasswordEmail from "../public-site/pages/ResetPasswordEmail";
 
 function AppRoutes() {
   return (
@@ -8,6 +9,7 @@ function AppRoutes() {
       <Route path='/' element={<Home />} />
       <Route path='/admin/login' element={<Login />} />
       <Route path='*' element={<Navigate to='/' replace />} />
+      <Route path='/reset-email-preview' element={<ResetPasswordEmail />} />
     </Routes>
   )
 }
