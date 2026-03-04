9e24839fa3cbc862d5a4f3041bcc4a1aa06ec250
nisa379 <delosalatony@gmail.com>
2026-03-04 16:52:20 +0700

    Design popular course and instructor

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes'
-
+import Popular_Courses from '../public-site/pages/Popular_Courses'
+import Instructor from '../public-site/pages/Instructor'
 function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <AppRoutes />
+    // </BrowserRouter>
+    <div>
+      <Popular_Courses></Popular_Courses>
+      <Instructor></Instructor>
+    </div>
   )
 }
 
--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,11 +1,12 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Home from '../public-site/pages/Home'
 import Login from '../dashboard/pages/auth/Login'
+import Popular_Courses from '../public-site/pages/Popular_Courses'
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Popular_Courses />} />
       <Route path='/admin/login' element={<Login />} />
       <Route path='*' element={<Navigate to='/' replace />} />
     </Routes>
