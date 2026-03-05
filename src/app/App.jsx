79c3d8105e92d0e601a45249f6b65477d6f111f5
nisa379 <delosalatony@gmail.com>
2026-03-05 15:21:15 +0700

    change route in routes.jsx and App.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,16 +1,11 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes'
-import Popular_Courses from '../public-site/pages/Popular_Courses'
-import Instructor from '../public-site/pages/Instructor'
+
 function App() {
   return (
-    // <BrowserRouter>
-    //   <AppRoutes />
-    // </BrowserRouter>
-    <div>
-      <Popular_Courses></Popular_Courses>
-      <Instructor></Instructor>
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -2,14 +2,17 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import Home from '../public-site/pages/Home'
 import Login from '../dashboard/pages/auth/Login'
 import Popular_Courses from '../public-site/pages/Popular_Courses'
+import Instructor from '../public-site/pages/Instructor'
+import ResetPasswordEmail from '../public-site/pages/ResetPasswordEmail'
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path='/' element={<Popular_Courses />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/design_popular_course_and_instructor' element={<><Popular_Courses /><Instructor /></>} />
       <Route path='/admin/login' element={<Login />} />
-      <Route path='*' element={<Navigate to='/' replace />} />
       <Route path='/reset-email-preview' element={<ResetPasswordEmail />} />
+      <Route path='*' element={<Navigate to='/' replace />} />
     </Routes>
   )
 }
