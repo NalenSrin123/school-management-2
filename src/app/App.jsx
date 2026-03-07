5d5aee21334cf3e3de9ac41c9d70fa700487c68b
Leeheng22 <kokkokoii38@gmail.com>
2026-03-07 22:54:27 +0700

    add DesignContactPageBottomSection.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter } from 'react-router-dom'
 // import AppRoutes from './routes'
 import Confim_reset_password from "../services/confim_reset_password";
+import DesingContactPageBottomSection from '../components/DesignContactPageBottomSection';
 function App() {
   return (
     // <BrowserRouter>
     //   <AppRoutes />
     // </BrowserRouter>
-    <Confim_reset_password></Confim_reset_password>
-    
+    <>
+      <Confim_reset_password></Confim_reset_password>
+      <DesingContactPageBottomSection/>
+    </>
   )
 }
 
