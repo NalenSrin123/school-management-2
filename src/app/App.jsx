6528f536c7d81f7678d33839834c910899c62d8f
Horn Ratana <honkimhan20@gmail.com>
2026-03-07 13:39:15 +0700

    Resigter Form

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes'
-
+import RegisterForm from '../dashboard/components/forms/register_form.jsx'
 function App() {
   return (
     <BrowserRouter>
-      <AppRoutes />
+      <RegisterForm/>
     </BrowserRouter>
   )
 }
