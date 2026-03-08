2538b163abf9e3435bdcb7d6b0c90aef806063b8
Ly Leanghour <Boyhour979@gmail.com>
2026-03-08 13:30:29 +0700

    Design_page_forgot_password

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes'
+import Design_page_forgot_password from "../services/Design_page_forgot_password";
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+      <Design_page_forgot_password />
+    </>
   )
 }
 
