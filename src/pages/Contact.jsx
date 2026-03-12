03bcd99a759c58adb79f27adee23d495c3dffb4a
Pichphearo <pichphearok@gmail.com>
2026-03-12 21:11:42 +0700

    Prepare Navigation Bar

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Contacts from "../public-site/pages/Contact";
+import DesingContactPageBottomSection from "../components/DesignContactPageBottomSection";
+
+const Contact = () => {
+  return (
+    <>
+      <Contacts />
+      <DesingContactPageBottomSection />
+    </>
+  );
+};
+
+export default Contact;
