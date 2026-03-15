2e264272a20d68ba1e5693800ad79020db9e6a88
Phea Minea <mineaphea737@gmail.com>
2026-03-15 15:39:58 +0700

    prepare homepage layout

--- src/components/PrepareHomePage.jsx
+++ src/components/PrepareHomePage.jsx
@@ -3,7 +3,6 @@ import Nav from './layout/nav'
 import HeroSection from '../public-site/components/HeroSection'
 import Popular_Courses from '../public-site/pages/Popular_Courses'
 import Instructor from '../public-site/pages/Instructor'
-import Footer from '../public-site/layout/Footer'
 
 const PrepareHomePage = () => {
   return (
@@ -12,7 +11,6 @@ const PrepareHomePage = () => {
         <HeroSection />
         <Popular_Courses />
         <Instructor />
-        <Footer />
     </div>
   )
 }
